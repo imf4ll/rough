c4d66b2623135d00a3fa430a9d07b4eeb842fb22
z3oxs <z3ox1s@protonmail.com>
2022-07-27 06:19:56 -0300

    feat: Added initial stage of searching

--- src/app.rs
+++ src/app.rs
@@ -1,22 +1,22 @@
 use std::process::{Command, exit};
 
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Entry};
+use gtk::{Application, ApplicationWindow, Entry, Box, Orientation, ListBox, Label, ScrolledWindow};
+use crate::utils::get_apps::get;
 
 pub fn handler(app: &Application) {
     app.connect_activate(| app | {
         let window = ApplicationWindow::builder()
             .application(app)
             .default_width(600)
-            .default_height(40)
-            .title("Rough App Launcher")
+            .title("Rough")
             .resizable(false)
             .has_focus(true)
             .build();
 
-        let textbox = Entry::new();
+        let container = Box::new(Orientation::Vertical, 0);
 
-        textbox.set_placeholder_text(Some("Type your command"));
+        let textbox = Entry::new();
 
         textbox.connect_key_press_event(| textbox, event | {
             if event.keycode() == Some(36) {
@@ -32,8 +32,68 @@ pub fn handler(app: &Application) {
                     .expect("An error was occurred, quitting...");
 
                 exit(0);
-            
-            } else if event.keycode() == Some(9) {
+            }
+
+            Inhibit(false)
+        });
+
+        let list = ListBox::new();
+        let apps = get("/usr/share/applications");
+
+        for app in &apps {
+            if app.generic != "" {
+                list.add(&Label::new(Some(&format!("{} ({})", &app.name, &app.generic))));
+
+            } else {
+                list.add(&Label::new(Some(&app.name)));
+
+            }
+        }
+
+        list.connect_row_selected(move | select, _ | {
+            let app = &apps[
+                select
+                    .selected_row()
+                    .expect("Failed to select row")
+                    .index() as usize
+            ];
+
+            let mut args = app
+                .exec
+                .split(" ")
+                .collect::<Vec<&str>>();
+
+            Command::new(args.swap_remove(0))
+                .args(args)
+                .spawn()
+                .expect("Failed to run command");
+
+            exit(0);
+        });
+
+        let scrollable = ScrolledWindow::builder()
+            .hscrollbar_policy(gtk::PolicyType::Never)
+            .min_content_height(250)
+            .child(&list)
+            .build();
+
+        container.add(&textbox);
+        container.add(&scrollable);
+    
+        unsafe {
+            textbox.connect_changed(move | text | {
+                &list.destroy();
+
+                let value = text.text();
+
+                &list.show_all();
+            });
+        }
+
+        window.add(&container);
+
+        window.connect_focus_out_event(| window, _ | {
+            if !window.has_focus() {
                 exit(0);
 
             }
@@ -41,12 +101,9 @@ pub fn handler(app: &Application) {
             Inhibit(false)
         });
 
-        window.add(&textbox);
-
-        window.connect_focus_out_event(| window, _ | {
-            if !window.has_focus() {
+        window.connect_key_press_event(| _, event | {
+            if event.keycode() == Some(9) {
                 exit(0);
-
             }
 
             Inhibit(false)
