91a72fd71165870f579f7eaab4ffe6fd6d916703
z3oxs <z3ox1s@protonmail.com>
2022-07-27 21:02:17 -0300

    feat: Added clap support

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,24 @@ mod utils;
 use gtk::prelude::*;
 use gtk::Application;
 use app::handler;
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(version, about = "A GTK based app launcher")]
+struct Args {
+    /// Run in shell mode
+    #[clap(short, long, takes_value = false)]
+    shell: bool
+}
 
 fn main() {
+    let args = Args::parse();
+   
     let app = Application::builder()
         .application_id("com.z3oxs.rough")
         .build();
 
-    handler(&app);
+    handler(&app, args.shell);
 
-    app.run();
+    app.run_with_args(&[""]);
 }
