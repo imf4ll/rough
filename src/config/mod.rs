55d1de52fedfa958bfb1978ad440647886a05fbb
z3oxs <z3ox1s@protonmail.com>
2022-07-29 19:26:35 -0300

    feat: Improved personalization config

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -2,25 +2,42 @@ use std::fs::read_to_string;
 use std::env::var;
 
 use serde_json;
-use crate::utils::types::Config;
+use crate::utils::types::{Config, Window, Container, TextBox, List};
 
 pub fn parse() -> Config {
+    let default_config = Config {
+        window: Window {
+            width: 600,
+            border_width: 6,
+            border_color: String::from("#252525"),
+        },
+        container: Container {
+            max_height: 200,
+        },
+        textbox: TextBox {
+            margin: 0,
+        },
+        list: List {
+            margin_top: 6,
+        }
+    };
+
     let path = match var("HOME") {
         Ok(path) => path,
-        Err(_) => return Config {
-            window_width: 600,
-            box_height: 200,
-        },
+        Err(_) => return default_config,
     };
 
     let content = match read_to_string(&format!("{}/.config/rough/config.json", path)) {
         Ok(text) => text,
-        Err(_) => return Config {
-            window_width: 600,
-            box_height: 200,
-        },
+        Err(_) => return default_config,
     };
 
-    serde_json::from_str::<Config>(&content)
-        .expect("Failed to read config file")
+    match serde_json::from_str::<Config>(&content) {
+        Ok(cfg) => cfg,
+        Err(_) => {
+            println!("\x1b[31m[X] Failed to load configuration file, using default config...\x1b[m");
+
+            default_config
+        }
+    }
 }
