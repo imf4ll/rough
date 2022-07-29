a1609b3f9435b1bf1311f6aa8269369f9f34c87f
z3oxs <z3ox1s@protonmail.com>
2022-07-29 19:28:25 -0300

    feat: Improved personalization config

--- src/utils/types.rs
+++ src/utils/types.rs
@@ -1,14 +1,39 @@
 use serde::Deserialize;
 
-#[derive(Deserialize, Debug)]
-pub struct Config {
-    pub window_width: i32,
-    pub box_height: i32,
-}
-
 #[derive(Debug, Clone)]
 pub struct App {
     pub name: String,
     pub generic: String,
     pub exec: String,
+}
+
+// CONFIGURATION
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    pub window: Window,
+    pub container: Container,
+    pub textbox: TextBox,
+    pub list: List,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Window {
+    pub width: i32,
+    pub border_width: i32,
+    pub border_color: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Container {
+    pub max_height: i32,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct TextBox {
+    pub margin: i32,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct List {
+    pub margin_top: i32,
 }
