325a716274e63b5dbcda3a15c9bfa24894d955cc
z3oxs <z3ox1s@protonmail.com>
2022-07-31 23:38:33 -0300

    feat: Added some customizations

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -2,7 +2,13 @@ use std::fs::read_to_string;
 use std::env::var;
 
 use serde_json;
-use crate::utils::types::{Config, Window, Container, TextBox, List};
+use crate::utils::types::{
+    Config,
+    Window,
+    Container,
+    TextBox,
+    List
+};
 
 pub fn parse() -> Config {
     let default_config = Config {
@@ -10,15 +16,20 @@ pub fn parse() -> Config {
             width: 600,
             border_width: 6,
             border_color: String::from("#252525"),
+            opacity: 1.0,
+            background_color: String::from("255, 255, 255"),
+            font_color: String::from("#FFFFFF"),
         },
         container: Container {
             max_height: 200,
         },
         textbox: TextBox {
             margin: 0,
+            transparent: false
         },
         list: List {
             margin_top: 6,
+            transparent: false
         }
     };
 
