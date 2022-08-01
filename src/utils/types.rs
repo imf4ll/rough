66f8318ca841535759729e52bbe20cd5c3a797a8
z3oxs <z3ox1s@protonmail.com>
2022-07-31 23:39:02 -0300

    feat: Added clone trait to configuration

--- src/utils/types.rs
+++ src/utils/types.rs
@@ -5,10 +5,11 @@ pub struct App {
     pub name: String,
     pub generic: String,
     pub exec: String,
+    pub icon: String,
 }
 
 // CONFIGURATION
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 pub struct Config {
     pub window: Window,
     pub container: Container,
@@ -16,24 +17,29 @@ pub struct Config {
     pub list: List,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 pub struct Window {
     pub width: i32,
     pub border_width: i32,
     pub border_color: String,
+    pub opacity: f64,
+    pub background_color: String,
+    pub font_color: String,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 pub struct Container {
     pub max_height: i32,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 pub struct TextBox {
     pub margin: i32,
+    pub transparent: bool,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 pub struct List {
     pub margin_top: i32,
+    pub transparent: bool,
 }
