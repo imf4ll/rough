80f50511d2bdaf83daea7f91e5bb9c9dd9314778
z3oxs <z3ox1s@protonmail.com>
2022-08-21 17:56:03 -0300

    feat: Added OOP to "styling" utility

--- src/styling/mod.rs
+++ src/styling/mod.rs
@@ -1,21 +1,73 @@
-mod utils;
-
 use gtk::prelude::*;
 use gtk::CssProvider;
 use crate::utils::types::Config;
-use utils::*;
 
-pub fn provider(config: &Config) -> CssProvider {
-    let mut css: String = create_default_css(config);
+pub struct Provider {
+    pub config: Config
+}
 
-    if config.window.opacity < 1.0 {
-        css = create_transparent_css(config);
-    
+impl Provider {
+    pub fn new(&self) -> CssProvider {
+        let mut css: String = Self::create_default_css(&self.config);
+
+        if self.config.window.opacity < 1.0 {
+            css = Self::create_transparent_css(&self.config);
+        
+        }
+
+        let provider = gtk::CssProvider::new();
+        
+        provider.load_from_data(css.as_bytes()).expect("Failed to read custom CSS");
+        
+        provider
     }
 
-    let provider = gtk::CssProvider::new();
-    
-    provider.load_from_data(css.as_bytes()).expect("Failed to read custom CSS");
-    
-    provider
+    fn create_transparent_css(config: &Config) -> String {
+        format!("
+            * {{
+                color: {};
+            }}
+
+            window {{
+                border: 1px solid {};
+            }}
+
+            entry {{
+                border: none;
+                font-weight: 500;
+                border-radius: 0;
+                padding: 0;
+                background: transparent;
+            }}
+
+            list {{
+                background: transparent;
+            }}
+        ",
+            config.window.font_color,
+            config.window.border_color,
+        )
+    }
+
+    fn create_default_css(config: &Config) -> String {
+        format!("
+            * {{
+                color: {};
+            }}
+
+            window {{
+                border: 1px solid {};
+            }}
+
+            entry {{
+                border: none;
+                font-weight: 500;
+                border-radius: 0;
+                padding: 0;
+            }}
+        ",
+            config.window.font_color,
+            config.window.border_color,
+        )
+    }
 }
